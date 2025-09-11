61f58e025b4dd2ffa30242c07ae486067028b6ba
morongwa236-MH <morongwa236@gmail.com>
2025-09-11 08:36:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbxOOehUGbS-CCZG6J5PBS0monRtcA8uuT5wUKLjrgQKjXzKMVTB-9DeX3DDDsl-LESM/exec";
+
+// Save household
+async function saveHousehold(blockName, address, contact) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      type: "household",
+      blockName,
+      address,
+      mainContact: contact
+    })
+  });
+  return response.json();
+}
+
+// Save member
+async function saveMember(householdId, memberData) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      type: "member",
+      householdId,
+      ...memberData
+    })
+  });
+  return response.json();
+}
+
+// Save child
+async function saveChild(householdId, childData) {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      type: "child",
+      householdId,
+      ...childData
+    })
+  });
+  return response.json();
+}
+
+// Fetch all records (for admin)
+async function getAllRecords() {
+  const response = await fetch(API_URL);
+  return response.json();
+}
 
